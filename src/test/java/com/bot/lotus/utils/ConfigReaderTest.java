b816b28103de8f4c7a2f0cae747f24745333e6d1
Henrietta <h_henrietta@outlook.com>
2022-10-16 21:14:04 +0200

    Corrected commented mistakes

--- src/test/java/com/bot/lotus/utils/ConfigReaderTest.java
+++ src/test/java/com/bot/lotus/utils/ConfigReaderTest.java
@@ -1,76 +1,100 @@
 package com.bot.lotus.utils;
 
-import static org.junit.Assert.*;
-
 import java.util.Collections;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 import java.io.IOException;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ConfigReaderTest {
-	String nonExistentFilePath =  "doesntExist.txt";
-	String notTxtorPropertiesFile = "test.zip";
-	String emptyTxtFile = "test.txt";
 	
-	public void readFileTestFunction(String fileToGive) {
-		File file = new File(fileToGive);
-		try {
-			if (file.createNewFile()) {
-			    assertEquals(Collections.emptyList(), ConfigReader.readFile(fileToGive) );
-			    file.delete();
-			}
-		} catch (IOException e) {
-			System.out.println("An error occurred.");
-			e.printStackTrace();
-		}
-	
-	}
-	
-	public void readPropertiesTestFunction(String fileToGive) {
-		File file = new File(fileToGive);
-		try {
-			if (file.createNewFile()) {
-			    assertEquals(Collections.emptyList(), ConfigReader.readFile(fileToGive) );
-			    file.delete();
-			}
-		} catch (IOException e) {
-			System.out.println("An error occurred.");
-			e.printStackTrace();
-		}
-	
-	}
-	@Test
-    public void readFileShouldReturnEmptyListIfFilePathDoesNotExist () {
-        assertEquals(Collections.emptyList(), ConfigReader.readFile(nonExistentFilePath) );
+	@Test(expected = NullPointerException.class) // Then
+    public void readFileShouldReturnExceptionIfFilePathDoesNotExist () {
+		// Given:
+        String nonExistentFilePath = "doesntExist.txt";
         
-    }
+        // When:
+        ConfigReader.readFile(nonExistentFilePath);
+	   }
 	
 	@Test
     public void readFileShouldReturnEmptyListIfFileIsNotTxt () {
-		readFileTestFunction(notTxtorPropertiesFile);
-		}
+		// Given:
+		String filePath = "test.zip";
+		File file = new File(filePath);
+		var expected = Collections.emptyList();
+		
+		try {
+			if (file.createNewFile()) {
+				// When: 
+				var actual = ConfigReader.readFile(filePath);
+				
+				// Then: 
+			    assertEquals(expected, actual);
+			    file.delete();
+			}
+		} catch (IOException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}}
 	
 	@Test
     public void readFileShouldReturnEmptyListIfFileIsAnEmptyTxt () {
-		readFileTestFunction(emptyTxtFile);
+		// Given:
+		String emptyTxtFilePath = "test.zip";
+		File file = new File(emptyTxtFilePath);
+		var expected = Collections.emptyList();
+		
+		
+		try {
+			if (file.createNewFile()) {
+				// When:
+				var actual = ConfigReader.readFile(emptyTxtFilePath);
+				
+				// Then:
+			    assertEquals(expected, actual);
+			    file.delete();
+			}
+		} catch (IOException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
     }
 	
+	@Test(expected = NullPointerException.class) // Then
+    public void readPropertiesShouldReturnExceptionIfFilePathDoesNotExist() {
+		// Given:
+		String nonExistentFilePath = "doesntexist.txt";
+		
+		// When:
+		ConfigReader.readProperties(nonExistentFilePath);
+	    }
+	
 	@Test
-    public void readPropertiesShouldReturnEmptyListIfFilePathDoesNotExist() {
-				
-		assertEquals(Collections.emptyList(), ConfigReader.readProperties(nonExistentFilePath) );
-    }
-	@Test
-    public void readPropertiesShouldReturnEmptyListIfFileIsNotTxt() {
-				
-		readPropertiesTestFunction(notTxtorPropertiesFile);
+    public void readPropertiesShouldReturnEmptyListIfFileIsNotProperties() {
+		// Given:
+		String filePath = "test.zip";
+		File file = new File(filePath);
+		var expected = Collections.emptyList();
+		
+		try {
+			if (file.createNewFile()) {
+				// When:
+			    var actual =  ConfigReader.readProperties(filePath);
+			    // Then:
+			    assertEquals(expected, actual);
+			    file.delete();
+			}
+		} catch (IOException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
     }
 	
 }
--- src/test/java/com/bot/lotus/utils/ValueFormatterTest.java
+++ src/test/java/com/bot/lotus/utils/ValueFormatterTest.java
@@ -2,15 +2,13 @@ package com.bot.lotus.utils;
 
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
-import static org.junit.Assert.*;
 
-import java.util.Collections;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 import java.io.IOException;
@@ -18,71 +16,76 @@ import java.io.IOException;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ValueFormatterTest {
-	String resultShouldBe = "12345";
 	
 	@Test
     public void shouldReturnWithoutWhitecpacesAndHufIfExpectedValueIsGiven () {
-		
-        String value = "12345";
-        
-        String result = ValueFormatter.formatValue(value);
-        
-        assertEquals(resultShouldBe, result );
+		// Given:
+        String valueToFormat = "12345";
+        String expected = "12345";
+        // When:
+        String actual = ValueFormatter.formatValue(valueToFormat);
+        // Then:
+        assertEquals(actual, expected);
         
     }
 	
 	@Test
     public void shouldReturnWithoutWhitecpacesAndHufIfWhitespaceAtTheBeginAndEnd () {
-		
-        String value = " 12345 ";
-       
-        String result = ValueFormatter.formatValue(value);
-        
-        assertEquals(resultShouldBe, result );
+		// Given:
+        String valueToFormat = " 12345 ";
+        String expected = "12345";
+        // When:
+        String result = ValueFormatter.formatValue(valueToFormat);
+        // Then:
+        assertEquals(expected, result);
         
     }
 	
 	@Test
     public void shouldReturnWithoutWhitecpacesAndHufIfWhitespaceInMiddleAndHuf () {
-		
-        String value = "12345 Ft";
-
-        String result = ValueFormatter.formatValue(value);
-        
-        assertEquals(resultShouldBe, result );
+		// Given:
+        String valueToFormat = "12345 Ft";
+        String expected = "12345";
+        // When:
+        String result = ValueFormatter.formatValue(valueToFormat);
+        // Then:
+        assertEquals(expected, result );
         
     }
 	
 	@Test
     public void shouldReturnWithoutWhitecpacesAndHufIfWhitespaceInMiddleAndHufAndWhitespaceAtBeginAndEnd () {
-		
-        String value = " 12345 Ft ";
-
-        String result = ValueFormatter.formatValue(value);
-        
-        assertEquals(resultShouldBe, result );
+		// Given:
+        String valueToFormat = " 12345 Ft ";
+        String expected = "12345";
+        // When:
+        String result = ValueFormatter.formatValue(valueToFormat);
+        // Then:
+        assertEquals(expected, result );
         
     }
 	
 	@Test
     public void shouldReturnWithoutWhitecpacesAndHufIfWhitespaceInMiddleAndHufIsLowercase () {
-		
-        String value = " 12345 ft ";
-
-        String result = ValueFormatter.formatValue(value);
-        
-        assertEquals(resultShouldBe, result );
+		// Given:
+        String valueToFormat = " 12345 ft ";
+        String expected = "12345";
+        // When:
+        String result = ValueFormatter.formatValue(valueToFormat);
+        // Then:
+        assertEquals(expected, result );
         
     }
 	
 	@Test
     public void shouldReturnWithoutWhitecpacesAndHufIfRandomWhitespaceInValue () {
-		
-        String value = "12 345";
-
-        String result = ValueFormatter.formatValue(value);
-        
-        assertEquals(resultShouldBe, result );
+		// Given:
+        String valueToFormat = "12 345";
+        String expected = "12345";
+        // When:
+        String result = ValueFormatter.formatValue(valueToFormat);
+        // Then:
+        assertEquals(expected, result );
         
     }
 	
