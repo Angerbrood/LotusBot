11dfd1ed93387f5609f0f01db2dd3423872f8ec3
David Petz <david_petz@epam.com>
2022-10-24 08:42:08 +0200

    Fixed build script

--- buildAll.bash
+++ buildAll.bash
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "Coping database from target"
+cd target/
+cp cache.db ../cache.db
+cd ..
 echo "Starting to build project"
 mvn clean compile assembly:single -DskipTests
 echo "Copying database to target folder"
