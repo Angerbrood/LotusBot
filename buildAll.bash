bfba46fb6fe57a11fddc790ce1093b868b72c100
David Petz <petzdavid@gmail.com>
2022-11-03 09:45:07 +0100

    Updated build script

--- buildAll.bash
+++ buildAll.bash
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-echo "Coping database from target"
-cd target/
-cp cache.db ../cache.db
-cd ..
 echo "Starting to build project"
 mvn clean compile assembly:single -DskipTests
-echo "Copying database to target folder"
+echo "Coping database from Anton"
+scp -P 65410 angerbrood@angerbrood.synology.me:~/Project/LotusBot/cache.db /d/Projects/LotusBot/
 cp cache.db target/cache.db
 cd target/
 echo "Starting Bot"
 java -jar lotus-webshop-bot-docker-jar-with-dependencies.jar
 cd ..
+echo "Copying database to Anton"
+scp -P 65410 cache.db angerbrood@angerbrood.synology.me:~/Project/LotusBot/
