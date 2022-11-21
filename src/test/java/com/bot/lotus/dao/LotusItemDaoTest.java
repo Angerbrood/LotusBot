9e2da71ffe9649676e42cd7725990274898ef56f
Henrietta <h_henrietta@outlook.com>
2022-11-21 20:18:39 +0100

    Added LotusItemDaoTest

--- src/test/java/com/bot/lotus/dao/LotusItemDaoTest.java
+++ src/test/java/com/bot/lotus/dao/LotusItemDaoTest.java
@@ -1,6 +1,9 @@
 package com.bot.lotus.dao;
 
 import com.bot.lotus.model.LotusItem;
+
+import jakarta.persistence.Query;
+
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.junit.Before;
@@ -10,9 +13,15 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.hibernate.Session;
+import java.util.List;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 
 @RunWith(MockitoJUnitRunner.class)
 public class LotusItemDaoTest {
@@ -32,12 +41,18 @@ public class LotusItemDaoTest {
         // given
         final Session session = Mockito.mock(Session.class);
         final Transaction transaction = Mockito.mock(Transaction.class);
-
+        final Query query = Mockito.mock(Query.class);
+        
         when(sessionFactory.openSession()).thenReturn(session);
         when(session.beginTransaction()).thenReturn(transaction);
+        when(session.createQuery(any(), any())).thenReturn(query);
+        when(query.list()).thenReturn(new ArrayList<>());
+        
         
         // when
-
+		underTest.findAll();
+		
         // then
-    }
+		
+		}
 }
